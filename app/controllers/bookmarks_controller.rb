7bbe8cf53f834567c3d94fc357d4156d5fef7100
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-05-26 15:57:16 -0400

    bookmarks : new, create and delete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,6 +19,8 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
